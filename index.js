dc82289f356e53e890603558c6299403aec2e1a4
Jemima Abu <jemimaabu@gmail.com>
2019-01-19 11:19:27 +0100

    Added formulas and rearranged function call

--- index.js
+++ index.js
@@ -4,40 +4,52 @@ var result = document.getElementById("result");
 
 var numbers = [];
 
-var randomFunctions = [add, multiply, even];
+// Array containing all random functions to be called. Function name must be included here
+var randomFunctions = [addSquares, twiceAdded,subtractSquare,fibonacci];
 
-function add(num1, num2) {
-    return num1 +  num2;
-}
-
-function multiply(num1,num2) {
-    return num1 *  num2;
-}
-
-function even(num1, num2) {
-    return 2*(num1-num2)
-}
-
-function power(num1, num2) {
-    return Math.pow(num1, num2)
-}
-
-function assignValues(randomFunction) {
-    var randomFirst = Math.floor(Math.random()*9);
+function addSquares() {
+    //Set first element as random number between 1 and 9
+    var randomFirst = Math.floor(Math.random()*9)+1;
+    // Push random number to array
     numbers.push(randomFirst);
-    for (var i = 0; i < 4; i++) {
-        if (i===0) {
-            if (randomFunction(numbers[i], 0) == 0) {
-                numbers.push(randomFunction(numbers[i], 1))
-            } else {
-                numbers.push(randomFunction(numbers[i], 0))
-            }
-        }
-        numbers.push(randomFunction(numbers[i], numbers[i+1]))
+    /*Write for loop sequence with number of elements that should be present in array 
+    Note that the displayPattern function removes the last element of the array so the length should be the numbers you want displayed + 1
+    i.e. if you want to display five numbers and the sixth number would be the one the user would be guessing, then you need to set i < 5*/
+    for (var i = 0; i < 5; i++) {
+        // Write function that returns number sequence according to formula
+        numbers.push(numbers[i]+Math.pow([i+1],2))
     }
 }
 
-assignValues(randomFunctions[Math.floor(Math.random()*randomFunctions.length)]);
+function twiceAdded() {
+    var randomFirst = Math.floor(Math.random()*9)+1;
+    numbers.push(randomFirst);
+    for (var i = 0; i < 5; i++) {
+        numbers.push((2*numbers[i])+(i+1))
+    }
+}
+
+function subtractSquare() {
+    //Not all number sequences require the first value to be random. This one, for example, just uses the position to iterate through the function
+    for (var i = 0; i < 5; i++) {
+        numbers.push((Math.pow(i+1,3))-(i+1))
+    }
+}
+
+function fibonacci() {
+    var randomFirst = Math.floor(Math.random()*9)+1;
+    numbers.push(randomFirst);
+    for (var i = 0; i < 5; i++) {
+        if(i===0) {
+            numbers.push(randomFirst+0)
+        } else {
+            numbers.push(numbers[i-1]+numbers[i])
+        }
+    }
+}
+
+const randomIndex = Math.floor(Math.random()*randomFunctions.length);
+randomFunctions[randomIndex]();
 
 function createSpan(element) {
     var span = document.createElement("span")
@@ -52,7 +64,7 @@ function displayPattern() {
 displayPattern();
 
 function guess() {
-    if (nextNumber.value == numbers[5]) {
+    if (nextNumber.value == numbers[numbers.length-1]) {
         result.innerHTML = "You win!"
     } else {
         result.innerHTML = "Nope, try again"
