736a35628500a97bab3bf46dcaacc07bf65b0cfd
Jemima Abu <jemimaabu@gmail.com>
2019-02-03 19:17:16 +0100

    Included difficulty level

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var nextNumber = document.getElementById("next-number");
 var display = document.getElementById("display-pattern");
 var result = document.getElementById("result");
 var nextButton = document.getElementById("next-button");
+var difficulty = document.getElementById("difficulty");
 
 /** The hint value shows up after the user has tried a sequence more than 3 times, if the function assigns a value to it */
 var hint = document.getElementById("hint");
 var tries = 0;
+var totalTries = 0;
+var win = 0;
 
 /** The numbers array gets its values from whichever function is called */
 var numbers = [];
@@ -27,7 +30,8 @@ function addSquares() {
         numbers.push(numbers[i]+Math.pow([i+1],2))
     }
     //Assign a value to 'hint' if you want to provide a hint, if not leave it blank
-    hint.innerHTML="Think of squares"
+    hint.innerHTML="Think of squares";
+    difficulty.className = "easy";
 }
 
 function twiceAdded() {
@@ -36,6 +40,7 @@ function twiceAdded() {
     for (var i = 0; i < 5; i++) {
         numbers.push((2*numbers[i])+(i+1))
     }
+    difficulty.className = "easy";
 }
 
 function subtractCube() {
@@ -44,7 +49,8 @@ function subtractCube() {
     for (i = randomFirst; i < randomFirst+5; i++) {
         numbers.push((Math.pow(i+1,3))-(i+1))
     }
-    hint.innerHTML = "Try raising to the 3rd power"
+    hint.innerHTML = "Try raising to the 3rd power";
+    difficulty.className = "medium";
 }
 
 function cubedAdd() {
@@ -52,7 +58,8 @@ function cubedAdd() {
     for (i = randomFirst; i < randomFirst+5; i++) {
         numbers.push((Math.pow(i+1,3))+((i+2)*(i+3)));
     }
-    hint.innerHTML = "Think of cubes"
+    hint.innerHTML = "Think of cubes";
+    difficulty.className = "medium";
 }
 
 function fibonacci() {
@@ -65,6 +72,7 @@ function fibonacci() {
             numbers.push(numbers[i-1]+numbers[i])
         }
     }
+    difficulty.className = "easy";
 }
 
 function exponential_one() {
@@ -75,6 +83,7 @@ function exponential_one() {
         numbers.push(num);
     }
     hint.innerHTML = "what do you know about exponential series";
+    difficulty.className = "hard";
 }
 
 function exponential_two() {
@@ -83,7 +92,8 @@ function exponential_two() {
     for (let i=0; i < 5; i++) {
         numbers.push(term(i));
     }
-    hint.innerHTML = "look again, think in powers"
+    hint.innerHTML = "look again, think in powers";
+    difficulty.className = "hard";
 }
 
 function exponential_three() {
@@ -95,6 +105,7 @@ function exponential_three() {
         numbers.push(rounded);
     }
     hint.innerHTML = "A number is raised to the index and divided by the index";
+    difficulty.className = "hard";;
 }
 
 function exponential_four() {
@@ -105,6 +116,7 @@ function exponential_four() {
         numbers.push(rounded);
     }
     hint.innerHTML = "An exponential of a number";
+    difficulty.className = "hard";
 }
 
 function log_e() {
@@ -114,6 +126,7 @@ function log_e() {
         numbers.push(toFixed(value, 2));
     }
     hint.innerHTML = "Find out a random number added to index and try some log on the it"
+    difficulty.className = "hard";
 }
 /** This function helps to round up numbers. I didn't rely on Math.round because it doesn't round up number e.g math.round(6.345) = 6.34 instead of 6.35 */ 
 function toFixed( num, precision ) {        
@@ -144,12 +157,14 @@ displayPattern();
 // Checks the users input against the last (hidden) element in the array
 function guess() {
     tries++;
+    totalTries++;
 
     if (tries == 3) {
         nextButton.style.display = "inline-block";
     }
 
     if (nextNumber.value == numbers[numbers.length-1]) {
+        win++;
         result.innerHTML = "You win!";
         result.style.color="#74d900";
         hint.innerHTML = "";
